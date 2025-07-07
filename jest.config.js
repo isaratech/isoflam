549333bbb76c265a5ca2793e29a5f3a5005ecf6b
thomas.martinet <thomas.martinet@isaratech.com>
2025-07-07 08:07:40 +0200

    feat: enhance Jest configuration with test matching and path ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,14 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   modulePaths: ['node_modules', '<rootDir>'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.(ts|tsx|js)',
+    '<rootDir>/src/**/?(*.)(spec|test).(ts|tsx|js)'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '\\.d\\.ts$'
+  ]
 };
