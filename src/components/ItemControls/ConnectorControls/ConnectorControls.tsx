a41cfcad37dda1b8b9346d4d4527687e8b8993e0
thomas.martinet <thomas.martinet@isaratech.com>
2025-07-21 18:23:28 +0200

    fix(ConnectorControls): add key prop to MenuItem for improved list rendering

--- src/components/ItemControls/ConnectorControls/ConnectorControls.tsx
+++ src/components/ItemControls/ConnectorControls/ConnectorControls.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import { Connector, connectorStyleOptions } from 'src/types';
-import {
-  Box,
-  Slider,
-  Select,
-  MenuItem,
-  TextField,
-  FormControlLabel,
-  Checkbox
-} from '@mui/material';
-import { useConnector } from 'src/hooks/useConnector';
-import { ColorSelector } from 'src/components/ColorSelector/ColorSelector';
-import { useUiStateStore } from 'src/stores/uiStateStore';
-import { useScene } from 'src/hooks/useScene';
-import { useTranslation } from 'src/hooks/useTranslation';
-import { ControlsContainer } from '../components/ControlsContainer';
-import { Section } from '../components/Section';
-import { DeleteButton } from '../components/DeleteButton';
-import { AdvancedSettings } from '../components/AdvancedSettings';
+import {Connector, connectorStyleOptions} from 'src/types';
+import {Box, Checkbox, FormControlLabel, MenuItem, Select, Slider, TextField} from '@mui/material';
+import {useConnector} from 'src/hooks/useConnector';
+import {ColorSelector} from 'src/components/ColorSelector/ColorSelector';
+import {useUiStateStore} from 'src/stores/uiStateStore';
+import {useScene} from 'src/hooks/useScene';
+import {useTranslation} from 'src/hooks/useTranslation';
+import {ControlsContainer} from '../components/ControlsContainer';
+import {Section} from '../components/Section';
+import {DeleteButton} from '../components/DeleteButton';
+import {AdvancedSettings} from '../components/AdvancedSettings';
 
 interface Props {
   id: string;
@@ -63,7 +55,7 @@ export const ConnectorControls = ({ id }: Props) => {
           }}
         >
           {Object.values(connectorStyleOptions).map((style) => {
-            return <MenuItem value={style}>{t(style)}</MenuItem>;
+              return <MenuItem key={style} value={style}>{t(style)}</MenuItem>;
           })}
         </Select>
       </Section>
